c989b8b6fb13f4c5bf23b0dc366406cdd835866c
programmerasif <mdasifkhan3757@gmail.com>
2023-05-02 21:09:56 +0600

    login page done

--- ph-assignment10/src/components/Login/Login.jsx
+++ ph-assignment10/src/components/Login/Login.jsx
@@ -14,7 +14,24 @@ const Login = () => {
     }
     return (
         <div className='w-md-25 w-sm-50 mx-auto mt-5 shadow p-3 mb-5 bg-body-tertiary rounded'>
-             login
+             <Form onSubmit={handelLogin}>
+             <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" name='email' required/>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" required placeholder="Password" name='password'/>
+      </Form.Group>
+      <Form.Group className="mb-3" >
+        Dont Have An Account? <Link to='/register'><span>Register</span></Link>
+      </Form.Group>
+      <button className='btn btn-danger w-100'>Log-in</button>
+    </Form>
         </div>
     );
 };
