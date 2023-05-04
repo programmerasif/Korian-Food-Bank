6b6d263a01d5800169ba77bd2313647a26071056
programmerasif <mdasifkhan3757@gmail.com>
2023-05-04 11:38:08 +0600

    hidde firebase config

--- ph-assignment10/src/firebase/firebase.config.js
+++ ph-assignment10/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjVOOTxunDNo-Y-2lj6YqQf9qRA01VSSY",
-  authDomain: "assignment10-e21db.firebaseapp.com",
-  projectId: "assignment10-e21db",
-  storageBucket: "assignment10-e21db.appspot.com",
-  messagingSenderId: "84744905373",
-  appId: "1:84744905373:web:8f41049d71a2a53c44ea74"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
